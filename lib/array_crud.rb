45558473b50949b1d16fceca10f34b4198a98dce
Jesse Brown <brownjesse8895@yahoo.com>
2020-12-24 23:23:11 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,45 @@
 def create_an_empty_array
-  
-end
+  []
+  end
 
 def create_an_array
+  ["Basketball", "Volleyball", "Kickball", "Softball"]
   
 end
 
 def add_element_to_end_of_array(array, element)
   
+  ["Basketball", "Volleyball", "Kickball", "Softball"] << "arrays!"
 end
+ 
 
 def add_element_to_start_of_array(array, element)
-  
+["Basketball", "Volleyball", "Kickball", "Softball"].unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
-  
+["Basketball", "Volleyball", "Kickball", "Softball", "arrays!"].pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  ["wow", "Basketball", "Volleyball", "Kickball", "Softball"].shift 
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  arrayname =["Basketball", "Volleyball", "am", "Kickball", "Softball"]
+  arrayname[2]
 end
 
 def retrieve_first_element_from_array(array)
+  arrayname = ["wow", "Basketball", "Volleyball","am" "Kickball", "Softball"].first
   
 end
 
 def retrieve_last_element_from_array(array)
-  
+  arrayname = ["wow", "Basketball", "Volleyball","am" "Kickball", "Softball", "arrays!"].last
 end
 
 def update_element_from_index(array, index_number, element)
-
+arrayname = ["wow", "Basketball", "Volleyball","am" "Kickball", "Softball", "arrays!"]
+arrayname[7] = "totally"
 end
